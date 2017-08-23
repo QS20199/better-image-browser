67c58eda2bbb8fcb9c682a92d92704af368af5f2
qsqiu <409745930@qq.com>
2017-08-24 00:29:50 +0800

    dev

--- dist/index.js
+++ dist/index.js
@@ -1,32 +1,67 @@
 const STEP = 1.25;
-const MIN_SCALE = 1 / Math.pow(STEP, 5);
+// const MIN_SCALE = 1 / Math.pow(STEP, 5);
 let img = document.querySelector('img');
-img.style.transform = 'scale(1) translate(0px, 0px)';
+img.style.transform = 'translate(0px, 0px)';
 img.draggable = false;
+img.style.width = img.width + 'px';
+img.style.height = img.height + 'px';
+function getPx(str) {
+    return str.split('px')[0];
+}
+let shouldMove = false, deltaX = 0, deltaY = 0;
+let timer;
+// todo: 重新设计方案
+// 关键步骤
+// 1.确定缩放重心
+// 2.确定4个关键值
 document.addEventListener('mousewheel', e => {
-    let scaleVal = Number(img.style.transform.match(/scale\((.*?)\)/)[1]);
+    let oldVal = {
+        width: getPx(img.style.width),
+        height: getPx(img.style.height),
+    }, newVal = {};
+    let transformStr = img.style.transform;
+    [, oldVal.tX, oldVal.tY] = transformStr.match(/translate\((.*?)px, (.*?)px\)/).map(v => Number(v));
+    // 求新的缩放值
     if (e.deltaY < 0) {
-        scaleVal *= STEP;
+        // newVal.scale = oldVal.scale * STEP;
+        newVal.width = oldVal.width * STEP;
+        newVal.height = oldVal.height * STEP;
     }
     else {
-        scaleVal /= STEP;
-        if (scaleVal < MIN_SCALE)
-            scaleVal = MIN_SCALE;
+        // newVal.scale = oldVal.scale / STEP;
+        newVal.width = oldVal.width / STEP;
+        newVal.height = oldVal.height / STEP;
+        // if (newVal.scale < MIN_SCALE) newVal.scale = MIN_SCALE;
     }
-    img.style.transform = img.style.transform.replace(/scale\(.*?\)/, `scale(${scaleVal})`);
+    let marginLeft = img.offsetLeft, marginTop = img.offsetTop;
+    // 实际上图片两边到left top的距离
+    let offsetX = img.offsetLeft + oldVal.tX, offsetY = img.offsetTop + oldVal.tY;
+    // 鼠标所在到图片两边的距离
+    let innerX = e.clientX - offsetX, innerY = e.clientY - offsetY;
+    console.log(innerX, innerY);
+    // 鼠标不在图片内, 位移量不变
+    if (innerX <= 0 || innerY <= 0 || innerX >= img.width || innerY >= img.height) {
+        newVal.tX = oldVal.tX;
+        newVal.tY = oldVal.tY;
+    }
+    else {
+        let sign = e.deltaY < 0 ? 1 : -1;
+        newVal.tX = oldVal.tX + sign * ((img.width / 2) - innerX) * (STEP - 1);
+        newVal.tY = oldVal.tY + sign * ((img.height / 2) - innerY) * (STEP - 1);
+    }
+    img.style.transform = `translate(${newVal.tX}px, ${newVal.tY}px)`;
+    img.style.width = newVal.width;
+    img.style.height = newVal.height;
 });
 document.addEventListener('click', e => {
     e.stopPropagation();
 }, true);
-let shouldMove = false, deltaX = 0, deltaY = 0;
-let timer;
 document.addEventListener('mousedown', e => {
     e.stopPropagation();
     shouldMove = true;
     img.style.transition = '0s';
     timer = setInterval(() => {
         let m = img.style.transform.match(/translate\((.*)\)/);
-        let scaleVal = Number(img.style.transform.match(/scale\((.*?)\)/)[1]);
         let x, y;
         if (m && m[1]) {
             [x, y] = m[1].split(',').map(v => Number(v.replace('px', '')));
@@ -34,8 +69,8 @@ document.addEventListener('mousedown', e => {
         else {
             x = y = 0;
         }
-        x = x + deltaX / scaleVal;
-        y = y + deltaY / scaleVal;
+        x = x + deltaX;
+        y = y + deltaY;
         img.style.transform = img.style.transform.replace(/translate\(.*?\)/, `translate(${x}px, ${y}px)`);
         deltaX = deltaY = 0;
     }, 16);
