a623e0c01abcf13869545a0db8faaf137d9f572a
邱佳文 <409745930@qq.com>
2017-10-17 17:04:18 +0800

    安装后自动打开demo页面

--- src/eventPage.ts
+++ src/eventPage.ts
@@ -1,3 +1,4 @@
+// 全屏事件监听
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
 	if (request && request.action === 'toggleFullScreen') {
 		chrome.windows.getCurrent(win => {
@@ -8,8 +9,9 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
 	}
 });
 
-//图标初始化
+
 chrome.storage.local.get(function (ret) {
+	// 图标初始化
 	if (ret.status !== "off") {
 		chrome.browserAction.setIcon({
 			path : "/asset/img/icon_128.png"
@@ -19,4 +21,16 @@ chrome.storage.local.get(function (ret) {
 			path : "/asset/img/icon_gray_128.png"
 		});
 	}
+
+	// 安装后打开demo页面
+	if (!ret.hasOpenDemoPage) {
+		chrome.tabs.create({
+			url: './src/demo.html'
+		})
+	} else {
+		chrome.storage.local.set({
+			hasOpenDemoPage: true
+		})
+	}
 })
+
